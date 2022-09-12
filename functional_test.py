a259c1bb17d69c7e47b6580fc83285cb52ce2c03
Paulo Bueno <paulob.bruno@gmail.com>
2022-09-12 00:38:36 -0300

    Added first unit test

--- functional_test.py
+++ functional_test.py
@@ -17,13 +17,10 @@ class NewVisitorTest(unittest.TestCase):
         # Check that it is the right page, called 'To-Do List'
         self.assertIn('To-Do List', self.browser.title)
         self.fail('Finish the test!')
-        
+
         # Register the first to-do item, in a blank space, hit enter and it creates a new line to add a new to-do item
-        
         # User sees a message that his to-do items were automatically saved
-        
         # Refresh the page to certify that the items were saved
-        
         # User closes the page
 
 if __name__ == '__main__':
