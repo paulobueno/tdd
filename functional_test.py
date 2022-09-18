deeb465894a421f6a749b84034eed474ae815364
Paulo Bueno <paulob.bruno@gmail.com>
2022-09-18 18:10:36 -0300

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,14 +17,40 @@ class NewVisitorTest(unittest.TestCase):
         # User opens the website
         self.browser.get('http://localhost:8000')
 
-        # Check that it is the right page, called 'To-Do List'
+        # Check that it is the right page, called 'To-Do List' and header 'To-Do'
         self.assertIn('To-Do List', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # Register the first to-do item, in a blank space, hit enter and it creates a new line to add a new to-do item
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Writes 'Buy peacock feathers' 
+        inputbox.send_keys('Buy peacock feathers')
+
+        # Hits enter and a new line is created
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         # User sees a message that his to-do items were automatically saved
+        self.fail('Finish the test!')
+
         # Refresh the page to certify that the items were saved
+
+
         # User closes the page
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
