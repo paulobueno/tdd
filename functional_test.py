08de47612b8848a7244b5216924cc4911a5d78e2
Paulo Bueno <paulob.bruno@gmail.com>
2022-11-12 19:18:12 -0300

    added post functionality and added tests

--- functional_test.py
+++ functional_test.py
@@ -25,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
-        # Register the first to-do item, in a blank space, hit enter and it creates a new line to add a new to-do item
+        # Register the first to-do item, in a blank space, hits enter, and it creates a new line to add a new to-do item
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
@@ -41,10 +41,19 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item not appearing in table."
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # Check if there still exists an input box
+        # Add second item and check if it was correctly registered
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
 
         # User sees a message that his to-do items were automatically saved
         self.fail('Finish the test!')
