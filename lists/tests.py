72eb66089170d4abd619a29156a8ea1a9f913f8e
Paulo Bueno <paulob.bruno@gmail.com>
2022-11-13 22:49:23 -0300

    added tests for Item's model

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -14,3 +15,20 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retreiving_items(self):
+        first_item = Item(text='The first (ever) list item')
+        first_item.save()
+
+        second_item = Item(text='Item the second')
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        record_values = [record.text for record in saved_items]
+        self.assertIn('The first (ever) list item', record_values)
+        self.assertIn('Item the second', record_values)
